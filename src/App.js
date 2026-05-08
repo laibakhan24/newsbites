c4420caade59fc0b4e54972e86241e0438dddf48
Laiba Khan <laibakhan21769@gmail.com>
2026-05-08 11:12:54 +0530

    Initial commit - NewsBites app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import './App.css';
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import { News } from './components/News';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
   pageSize = 16;
-
-  state = { progress: 0 }
-
+  state = {
+    progress: 0
+  }
   setProgress = (progress) => {
     this.setState({ progress: progress });
   }
-
   render() {
     return (
       <Router>
         <Navbar />
-        <LoadingBar color="#f11946" progress={this.state.progress} />
+        <LoadingBar
+          color="#f11946"
+          progress={this.state.progress}
+        />
         <Routes>
           <Route exact path="/" element={<News setProgress={this.setProgress} key={"general"} pageSize={this.pageSize} country="us" category="general" />} />
+          {/* <Route exact path="/about" element={<News setProgress={this.setProgress} key={"about"} pageSize={this.pageSize} country="us" category="about" />} /> */}
           <Route exact path="/business" element={<News setProgress={this.setProgress} key={"business"} pageSize={this.pageSize} country="us" category="business" />} />
           <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key={"entertainment"} pageSize={this.pageSize} country="us" category="entertainment" />} />
           <Route exact path="/health" element={<News setProgress={this.setProgress} key={"health"} pageSize={this.pageSize} country="us" category="health" />} />
