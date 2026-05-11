8812fbe4b1c19eabe5763c6e8526a58db88f5591
Laiba Khan <laibakhan21769@gmail.com>
2026-05-11 15:20:17 +0530

    Switch to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import { News } from './components/News';
-import {
-  HashRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
